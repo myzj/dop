43ae976330a9bbeb352c1290db30911df99beec2
刘刚 <262488346@qq.com>
2016-11-24 08:55:07 +0800

    no message

--- dop/dop/urls.py
+++ dop/dop/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from dop import views
+from settings import STATICFILES_DIRS
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^test/?$', views.test),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATICFILES_DIRS[0], 'show_indexes': True}),
+
 ]

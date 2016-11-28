754842ad4444cd789fe5ac659f5b6ec250a3d2e8
宗柱 <zsz1860@muyingzhijia.com>
2016-11-28 16:46:51 +0800

    no message

---
+++ dop/web/static/js/controller/atm/team.js
@@ -0,0 +1,7 @@
+require(['lib/common','model/login/loginModel'],
+    function($,loginModel){
+        /*var app = angular.module('my-pro',[]);
+        app.controller('teamList',function($scope){
+        $scope.msg='你好 hello';
+    })*/
+});

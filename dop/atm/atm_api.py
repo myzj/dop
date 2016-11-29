ccedb48fc58fd9303e1dc83726eebbf0ebbfcdea
刘刚 <262488346@qq.com>
2016-11-29 10:16:03 +0800

    Project和api查询支持keyword

--- dop/atm/atm_api.py
+++ dop/atm/atm_api.py
@@ -279,45 +279,45 @@ def req_project(request):
         'success': True, 'errorcode': 0, 'errormsg': '', 'result': {}}
     if request.method == 'GET':
         try:
-            params = request.GET.dict()
-            required_fields = ["team_id"]
-            errmsg = ''
-            for field in required_fields:
-                if field not in params or not params[field]:
-                    errmsg += field + ', '
-            if errmsg:
-                queryset['errorcode'] = 100001
-                queryset['errormsg'] = errmsg + ' ' + getMessage('100001')
-                return JSONResponse(queryset)
-            team_filter = Team.objects.filter(is_deleted=False, is_active=True, id=params.get("team_id"))
-            if team_filter:
-                project_match = Project.objects.filter(is_deleted=False, is_active=True, team=team_filter[0]).order_by(
-                    '-utime')
-                queryset['result']['pageIndex'] = 1
-                queryset['result']['pageSize'] = 10
-                queryset['result']['totalCount'] = project_match.count()
-                queryset['result']['projectList'] = []
-                # queryset['result']['pageCount'] = 0
-                if 'pageSize' in request.GET:
-                    queryset['result']['pageSize'] = request.GET['pageSize']
-                if 'pageIndex' in request.GET:
-                    queryset['result']['pageIndex'] = request.GET['pageIndex']
-                project_pages = paginator.Paginator(project_match, queryset['result']['pageSize'])
-                queryset['result']['pageCount'] = project_pages.num_pages
-                if project_match.count() > 0:
-                    project_page = project_pages.page(queryset['result']['pageIndex'])
-                    for project in project_page.object_list:
-                        result = {'project_name': project.project_name, 'project_id': project.id,
-                                  'ctime': project.ctime.strftime('%Y-%m-%d %H:%M:%S'), \
-                                  'project_pic_url': project.pic_url}
-                        queryset['result']['projectList'].append(result)
-                    return JSONResponse(queryset)
+            team_filter = None
+            project_match = None
+            if 'team_id' in request.GET and request.GET['team_id'] != '':
+                team_filter = Team.objects.filter(is_deleted=False, is_active=True, id=request.GET['team_id'])
+                if team_filter:
+                    project_match = Project.objects.filter(is_deleted=False, is_active=True, team=team_filter[0]).order_by(
+                        '-utime')
                 else:
+                    queryset['errorcode'] = 300019  # 找不到匹配team id的工程列表
+                    queryset['errormsg'] = getMessage('300019')
                     return JSONResponse(queryset)
+            # 输入关键词查询
             else:
-                queryset['errorcode'] = 300019
-                queryset['errormsg'] = errmsg + ' ' + getMessage('300019')
+                if 'keyword' in request.GET and request.GET['keyword'] != '':
+                    project_match = Project.objects.filter(is_deleted=False, is_active=True, project_name__icontains=request.GET['keyword']) \
+                        .order_by('-utime')
+                else:
+                    project_match = Project.objects.filter(is_deleted=False, is_active=True).order_by('-utime')
+            queryset['result']['pageIndex'] = 1
+            queryset['result']['pageSize'] = 10
+            queryset['result']['totalCount'] = project_match.count()
+            queryset['result']['projectList'] = []
+            if 'pageSize' in request.GET:
+                queryset['result']['pageSize'] = request.GET['pageSize']
+            if 'pageIndex' in request.GET:
+                queryset['result']['pageIndex'] = request.GET['pageIndex']
+            project_pages = paginator.Paginator(project_match, queryset['result']['pageSize'])
+            queryset['result']['pageCount'] = project_pages.num_pages
+            if project_match.count() > 0:
+                project_page = project_pages.page(queryset['result']['pageIndex'])
+                for project in project_page.object_list:
+                    result = {'project_name': project.project_name, 'project_id': project.id,
+                              'ctime': project.ctime.strftime('%Y-%m-%d %H:%M:%S'), \
+                              'project_pic_url': project.pic_url}
+                    queryset['result']['projectList'].append(result)
                 return JSONResponse(queryset)
+            else:
+                return JSONResponse(queryset)
+
         except BaseException, ex:
             except_info(ex)
             queryset['errorcode'] = 300021
@@ -447,43 +447,43 @@ def req_api_list(request):
         'success': True, 'errorcode': 0, 'errormsg': '', 'result': {}}
     if request.method == 'GET':
         try:
-            params = request.GET.dict()
-            required_fields = ["project_id"]
-            errmsg = ''
-            for field in required_fields:
-                if field not in params or not params[field]:
-                    errmsg += field + ', '
-            if errmsg:
-                queryset['errorcode'] = 100001
-                queryset['errormsg'] = errmsg + ' ' + getMessage('100001')
-                return JSONResponse(queryset)
-            project_filter = Project.objects.filter(is_deleted=False, is_active=True, id=params.get("project_id"))
-            # 判断项目是否存在
-            if project_filter:
-                api_match = Interface.objects.filter(is_deleted=False, is_active=True, project=project_filter[0])
-                queryset['result']['pageIndex'] = 1
-                queryset['result']['pageSize'] = 10
-                queryset['result']['totalCount'] = api_match.count()
-                queryset['result']['apiList'] = []
-                if 'pageSize' in request.GET:
-                    queryset['result']['pageSize'] = request.GET['pageSize']
-                if 'pageIndex' in request.GET:
-                    queryset['result']['pageIndex'] = request.GET['pageIndex']
-                api_pages = paginator.Paginator(api_match, queryset['result']['pageSize'])
-                queryset['result']['pageCount'] = api_pages.num_pages
-                if api_match.count() > 0:
-                    api_page = api_pages.page(queryset['result']['pageIndex'])
-                    for api in api_page.object_list:
-                        result = {'id': api.id, 'interface_name': api.interface_name, 'description': api.description, 'url': api.url, 'method': api.method, \
-                                  'content_type': api.content_type, 'remark': api.remark, 'tags': api.tags, \
-                                  'update_time': api.utime.strftime('%Y-%m-%d %H:%M:%S')}
-                        queryset['result']['apiList'].append(result)
-                    return JSONResponse(queryset)
+            project_filter = None
+            api_match = None
+            # 传入项目id
+            if 'project_id' in request.GET and request.GET['project_id'] != '':
+                project_filter = Project.objects.filter(is_deleted=False, is_active=True, id=request.GET['project_id'])
+                if project_filter:
+                    api_match = Interface.objects.filter(is_deleted=False, is_active=True, project=project_filter[0])
                 else:
+                    queryset['errorcode'] = 300025
+                    queryset['errormsg'] = getMessage('300025')
                     return JSONResponse(queryset)
             else:
-                queryset['errorcode'] = 300025
-                queryset['errormsg'] = errmsg + ' ' + getMessage('300025')
+                if 'keyword' in request.GET and request.GET['keyword'] != '':
+                    api_match = Interface.objects.filter(is_deleted=False, is_active=True, interface_name__icontains=request.GET['keyword']) \
+                        .order_by('-utime')
+                else:
+                    api_match = Interface.objects.filter(is_deleted=False, is_active=True).order_by('-utime')
+            # 判断项目是否存在
+            queryset['result']['pageIndex'] = 1
+            queryset['result']['pageSize'] = 10
+            queryset['result']['totalCount'] = api_match.count()
+            queryset['result']['apiList'] = []
+            if 'pageSize' in request.GET:
+                queryset['result']['pageSize'] = request.GET['pageSize']
+            if 'pageIndex' in request.GET:
+                queryset['result']['pageIndex'] = request.GET['pageIndex']
+            api_pages = paginator.Paginator(api_match, queryset['result']['pageSize'])
+            queryset['result']['pageCount'] = api_pages.num_pages
+            if api_match.count() > 0:
+                api_page = api_pages.page(queryset['result']['pageIndex'])
+                for api in api_page.object_list:
+                    result = {'id': api.id, 'interface_name': api.interface_name, 'description': api.description, 'url': api.url, 'method': api.method, \
+                              'content_type': api.content_type, 'remark': api.remark, 'tags': api.tags, \
+                              'update_time': api.utime.strftime('%Y-%m-%d %H:%M:%S')}
+                    queryset['result']['apiList'].append(result)
+                return JSONResponse(queryset)
+            else:
                 return JSONResponse(queryset)
         except BaseException, ex:
             except_info(ex)

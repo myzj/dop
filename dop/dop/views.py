25ab49cf0191817f641f53f94ada35a4e9c7ecb9
刘刚 <262488346@qq.com>
2016-11-30 14:38:05 +0800

    增加404页面 获取工程页面对应的team名称

--- dop/dop/views.py
+++ dop/dop/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from atm.atm_api import login_check
+from atm.models import Team
+from atm.common import except_info
 
 
 def test(request):
@@ -25,6 +27,19 @@ def teamlist(request):
 @login_check
 def projectlist(request):
     params = {}
+    try:
+        if request.GET and request.GET['team'] != '':
+            team_id = request.GET['team']
+            team_filter = Team.objects.filter(is_deleted=False, is_active=True, id=team_id)
+            if team_filter:
+                params['team_name'] = team_filter[0].team_name
+            else:
+                return render(request, '404.html', params)
+        else:
+            return render(request, '404.html', params)
+    except BaseException, ex:
+        except_info(ex)
+        return render(request, '404.html', params)
     params['title'] = '项目列表'
     return render(request, 'atm/projectlist.html', params)
 

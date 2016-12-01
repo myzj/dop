6c992736cc006405335306cbf6c47e7c71a20d3a
刘刚 <262488346@qq.com>
2016-12-01 14:07:42 +0800

    no message

--- dop/dop/context_process.py
+++ dop/dop/context_process.py
@@ -4,6 +4,9 @@ import config
 
 # 返回配置信息
 def get_config(request):
+    full_path = request.get_full_path()
+    last_path = full_path.split("?")
+    config.path = last_path[0]
     return {'config': config}
 
 

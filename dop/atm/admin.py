9286213eccf6ab452cdbac97bc63b658373227f3
wjh2018 <wjh2018@muyingzhijia.com>
2016-12-06 16:14:17 +0800

    update admin.py

--- dop/atm/admin.py
+++ dop/atm/admin.py
@@ -10,36 +10,43 @@ class TeamAdmin(admin.ModelAdmin):
 
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('id', 'project_name')
+    list_filter = ('team', 'author', 'modifier', 'is_active',)
     list_display = ('id', 'team', 'project_name', 'description', 'pic_url', 'host', 'remark', 'is_active', 'is_deleted', 'author', 'modifier', 'ctime', 'utime',)
 
 
 class ProjectMemberAdmin(admin.ModelAdmin):
     search_fields = ('id',)
+    list_filter = ('project', 'user', 'role', 'author', 'modifier',)
     list_display = ('id', 'project', 'user', 'role', 'author', 'modifier', 'is_active', 'is_deleted', 'ctime', 'utime',)
 
 
 class InterfaceAdmin(admin.ModelAdmin):
     search_fields = ('id', 'interface_name')
+    list_filter = ('project',)
     list_display = ('id', 'project', 'interface_name', 'description', 'url', 'method', 'content_type', 'tags', 'mockdata', 'remark', 'is_active', 'is_deleted', 'author', 'modifier', 'ctime', 'utime',)
 
 
 class MetaDataAdmin(admin.ModelAdmin):
     search_fields = ('id',)
+    list_filter = ('interface', 'position', 'author', 'modifier', 'is_active',)
     list_display = ('id', 'interface', 'position', 'metadata_name', 'data', 'remark', 'is_active', 'is_deleted', 'author', 'modifier', 'ctime', 'utime',)
 
 
 class ErrorCodeAdmin(admin.ModelAdmin):
     search_fields = ('id', 'error_name')
+    list_filter = ('interface', 'author', 'modifier', 'is_active',)
     list_display = ('id', 'interface', 'error_name', 'display_message', 'description', 'remark', 'is_active', 'is_deleted', 'author', 'modifier', 'ctime', 'utime',)
 
 
 class LockInfoAdmin(admin.ModelAdmin):
     search_fields = ('id', )
+    list_filter = ('interface', 'lock_user', 'is_locked',)
     list_display = ('id', 'interface', 'lock_user', 'is_locked', 'is_deleted', 'ctime', 'utime',)
 
 
 class EditHistoryAdmin(admin.ModelAdmin):
     search_fields = ('id', )
+    list_filter = ('interface', 'modifier',)
     list_display = ('id', 'interface', 'modifier', 'content', 'is_deleted', 'ctime', 'utime',)
 
 
